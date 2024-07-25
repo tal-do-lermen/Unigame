0b7661c00cb39058f7a531abad66fd948bd82e72
João Pedro Ritter Lermen <joaorl@unimedvaledocai.com.br>
2024-07-25 10:38:18 -0300

    animacao

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -1,39 +1,42 @@
 import React from "react";
 import { MapSize, TileSize } from "../cfg";
-import Debugger from "../Debugger";
 import Player from "../player/player";
 
-const posicaoInicial = { x: 0, y: 23 }
+const posicaoInicial = { x: 0, y: 46 }
 
 const Board = () => {
     const [playerPosicao, setPlayerPosicao] = React.useState(posicaoInicial);
     const [isLeft, setIsLeft] = React.useState(false);
+    const [animacaoAndamento, setAnimacaoAndamento] = React.useState(0);
     const [direcao, setDirecao] = React.useState(null);
     const refElement = React.useRef()
     const [map, setMap] = React.useState();
 
     const mouseClick = async (x, y) => {
-
+        // setCountAnimacao(countAnimacao + 1);
         let map = refElement.current.getBoundingClientRect();
         let clickX = Math.trunc((x - map.left) / TileSize)
         let clickY = Math.trunc((y - map.top) / TileSize)
-        console.log("x", clickX)
-        console.log("Y", clickY)
         let posicaoAtualX = Math.trunc((playerPosicao.x));
         let posicaoAtualY = Math.trunc((playerPosicao.y));
+        let animacao = undefined
+        let posicaoFinalX;
+        let posicaoFinalY;
+        
+            clearInterval(animacaoAndamento)
+        
+            animacao = setInterval((e) => {
 
-        let animacao = setInterval((e) => {
-
+                console.log(animacao)
             if (posicaoAtualX === clickX) {
 
 
-
                 if (clickY < posicaoAtualY) {
-                    posicaoAtualY--
+                    posicaoAtualY-= 0.5
                     setDirecao('C')
                 }
                 else {
-                    posicaoAtualY++
+                    posicaoAtualY+= 0.5
                     setDirecao('B')
                 }
                 setPlayerPosicao({ x: posicaoAtualX, y: posicaoAtualY })
@@ -41,11 +44,11 @@ const Board = () => {
 
                 setDirecao('ED')
                 if (clickX < posicaoAtualX) {
-                    posicaoAtualX--
+                    posicaoAtualX-= 0.5
                     setIsLeft(true)
                 }
                 else {
-                    posicaoAtualX++
+                    posicaoAtualX+= 0.5
                     setIsLeft(false)
                 }
                 setPlayerPosicao({ x: posicaoAtualX, y: posicaoAtualY })
@@ -53,10 +56,12 @@ const Board = () => {
             if (posicaoAtualY === clickY && posicaoAtualX === clickX) {
                 clearInterval(animacao)
                 setDirecao('S')
+                posicaoFinalX = posicaoAtualX
+                posicaoFinalY =posicaoAtualY
+                
             }
-            console.log(posicaoAtualX, posicaoAtualY)
             //ToDo fazer dinamicamente com retorno banco. Array?
-            if ((posicaoAtualX > 12 && posicaoAtualX < 17) && (posicaoAtualY > 38 && posicaoAtualX < 44)) {
+            if ((posicaoFinalX > 12 && posicaoFinalX < 17) && (posicaoFinalY > 35 && posicaoFinalY < 45)) {
                 return setMap(<div style={{
                     backgroundImage: "url(./assets/lgpd.png)",
                     width: MapSize,
@@ -68,15 +73,17 @@ const Board = () => {
             } else setMap()
 
         },
-            200
+            90
             , [setIsLeft, setDirecao, setPlayerPosicao, posicaoAtualY, posicaoAtualX, clickX, clickY])
-
+      
+      setAnimacaoAndamento(animacao)
+      
 
     }
 
     return (
         <div>
-
+           
             <div ref={refElement} style={{
                 width: MapSize,
                 height: MapSize,
@@ -86,7 +93,7 @@ const Board = () => {
             }}
                 onClick={e => { mouseClick(e.clientX, e.clientY) }}>
                 {map}
-                <Debugger />
+                {/* <Debugger></Debugger> */}
                 <Player position={playerPosicao} Direcao={direcao} isLeft={isLeft} />
             </div>
         </div>
